4bd6e78d62ce86de1f971b977a611cef648654cc
Lorenzo Tuppince <ltuppince@gmail.com>
2020-04-20 13:19:17 -0400

    Start with create-react-app

---
+++ client/build/precache-manifest.57e1ca141b6d8bc0bcc53eb48c5a3091.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "38faa4c96ad8bc5924eb",
+    "url": "/static/css/main.e314e914.chunk.css"
+  },
+  {
+    "revision": "38faa4c96ad8bc5924eb",
+    "url": "/static/js/main.f3c06501.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4bc15d4d204de824e77a",
+    "url": "/static/js/2.1669d26a.chunk.js"
+  },
+  {
+    "revision": "92a2ac48c12828857bb85a139b605aa6",
+    "url": "/static/media/Image 2-10-20 at 6.44 PM.92a2ac48.jpeg"
+  },
+  {
+    "revision": "aa50409cc5fd124a927839a3e7428225",
+    "url": "/static/media/Image 2-5-20 at 1.11 PM.aa50409c.jpg"
+  },
+  {
+    "revision": "20842517728d631e7be6f55741dffe86",
+    "url": "/static/media/IMG_2030.20842517.jpg"
+  },
+  {
+    "revision": "ef09b248e7826198329396104c0b22b4",
+    "url": "/static/media/IMG_2821.ef09b248.jpeg"
+  },
+  {
+    "revision": "4e9758f0cacb93ebf9c49218263fd2ef",
+    "url": "/static/media/IMG_2826.4e9758f0.jpeg"
+  },
+  {
+    "revision": "1d2f23a42f67607a208fa777b8085445",
+    "url": "/static/media/IMG_2831.1d2f23a4.jpeg"
+  },
+  {
+    "revision": "f1f935007d92a6db7ba1ba838a8e39a7",
+    "url": "/index.html"
+  }
+];
