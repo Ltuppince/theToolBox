1d87eb51bcd2af13940ccff756467171016b0798
Lorenzo Tuppince <ltuppince@gmail.com>
2020-04-29 23:22:57 -0400

    update logo

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -7,10 +7,12 @@ function About() {
       <div className="container">
       
       <p>
+        
         THE TOOL BOX is a company knowledgebase to centralize information,
         streamline processes, and homogenize communication across all business
         verticals. This is a complement to in-house training and mentorship for
         new associates and newly acquired organizations.
+          
       </p>
       </div>
     </div>
